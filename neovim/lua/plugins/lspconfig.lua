31d6909676de9a9a6c9c974071eb07f4cd3ff4d9
nekomangini <encar.luis.salazar@gmail.com>
2024-08-10 07:37:46 +0800

    config: added lsp

---
+++ neovim/lua/plugins/lspconfig.lua
@@ -0,0 +1,59 @@
+return { 
+  { 
+    "williamboman/mason.nvim",
+    config = function()
+      -- requires mason and calls setup
+      require("mason").setup()
+    end
+  },
+  { 
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function() 
+      local lspconfig = require('lspconfig')
+
+      -- rust
+      lspconfig.rust_analyzer.setup {
+        -- Server-specific settings. See `:help lspconfig-setup`
+        settings = {
+          ['rust-analyzer'] = {},
+        },
+      }
+      
+      -- lua
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            workspace = {
+              checkThirdParty = false,
+            },
+            codeLens = {
+              enable = true
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+            doc = {
+              privateName = { "^_" },
+            },
+            hint = {
+              enable = true,
+              setType = false,
+              parmType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
+            }
+          }
+        }
+      })
+    end
+  },
+}
