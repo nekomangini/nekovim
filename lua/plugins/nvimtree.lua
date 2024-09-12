7075a78d3302b8552a50bd36e928828fe91e1337
nekomangini <encar.luis.salazar@gmail.com>
2024-09-12 15:34:34 +0800

    config update

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -30,8 +30,8 @@ return {
         end
       end
 
-      -- open as vsplit on current node
-      local function vsplit_preview()
+      -- open as vsplit(vertical) on current node
+      local function vsplit_vertical()
         local node = api.tree.get_node_under_cursor()
 
         if node.nodes ~= nil then
@@ -40,6 +40,24 @@ return {
         else
           -- open file as vsplit
           api.node.open.vertical()
+          -- api.node.show_info_popup()
+        end
+
+        -- Finally refocus on tree if it was lost
+        api.tree.focus()
+      end
+
+      -- open as vsplit(horizontal) on current node
+      local function vsplit_horizontal()
+        local node = api.tree.get_node_under_cursor()
+
+        if node.nodes ~= nil then
+          -- expand or collapse folder
+          api.node.open.edit()
+        else
+          -- open file as vsplit
+          api.node.open.horizontal()
+          -- api.node.show_info_popup()
         end
 
         -- Finally refocus on tree if it was lost
@@ -48,7 +66,8 @@ return {
 
       -- On_attach mappings
       keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
-      keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+      keymap.set("n", "v", vsplit_vertical, opts("Vsplit Vertical"))
+      keymap.set("n", "V", vsplit_horizontal, opts("Vsplit Horizontal"))
       keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
       keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
     end
