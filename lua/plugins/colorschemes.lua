c094cafebf36f0ba0d4ad1212e5c64759f964644
nekomangini <encar.luis.salazar@gmail.com>
2024-09-14 10:47:08 +0800

    change theme to use bamboo colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,14 +1,13 @@
 return {
-
   -- tokyonight
   {
     "folke/tokyonight.nvim",
-    lazy = false,
     priority = 1000,
+    lazy = false,
     opts = {
       style = "night",        -- the theme comes in three styles, 'storm', a darker variant 'night' and 'day'
       -- light_style = "day",    -- the theme is used when the background is set to light
-      transparent = true,    -- enable this to disable setting the background color
+      transparent = true,     -- enable this to disable setting the background color
       terminal_colors = true, -- configure the colors used when opening a `:terminal` in Neovim
       styles = {
         -- style to be applied to different syntax groups
@@ -21,7 +20,6 @@ return {
         sidebars = "transparent",
         floats = "dark",
       },
-
     },
   },
 
@@ -58,6 +56,7 @@ return {
   -- catppuccin
   {
     "catppuccin/nvim",
+    priority = 1000,
     lazy = true,
     name = "catppuccin",
     opts = {
@@ -132,5 +131,58 @@ return {
         which_key = true,
       },
     },
+  },
+
+  -- bamboo theme
+  {
+    "ribru17/bamboo.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("bamboo").setup({
+        -- optional configuration here
+        -- Main options --
+        -- NOTE: to use the light theme, set `vim.o.background = 'light'`
+        style = "multiplex",                                      -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
+        toggle_style_key = nil,                                   -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
+        toggle_style_list = { "vulgaris", "multiplex", "light" }, -- List of styles to toggle between
+        transparent = true,                                       -- Show/hide background
+        dim_inactive = false,                                     -- Dim inactive windows/buffers
+        term_colors = true,                                       -- Change terminal color as per the selected theme style
+        ending_tildes = false,                                    -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false,                             -- reverse item kind highlights in cmp menu
+
+        -- Change code style ---
+        -- Options are anything that can be passed to the `vim.api.nvim_set_hl` table
+        -- You can also configure styles with a string, e.g. keywords = 'italic,bold'
+        code_style = {
+          comments = { italic = true },
+          conditionals = { italic = true },
+          keywords = {},
+          functions = {},
+          namespaces = { italic = true },
+          parameters = { italic = true },
+          strings = {},
+          variables = {},
+        },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+        -- Custom Highlights --
+        colors = {},     -- Override default colors
+        highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = false,    -- darker colors for diagnostic
+          undercurl = true,  -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      })
+      require("bamboo").load()
+    end,
   },
 }
