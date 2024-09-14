3cf6d3423feac269db39e594b0a6ef81b081ffed
nekomangini <encar.luis.salazar@gmail.com>
2024-09-14 10:45:26 +0800

    added autocompletion

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,13 +10,41 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer", "volar", "emmet_ls" },
+        -- automatically install a language 
+        -- auto_install = true,
+        ensure_installed = { "lua_ls", "rust_analyzer" },
       })
     end,
   },
 
   {
     "neovim/nvim-lspconfig",
+    lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+
+      -- autocompletion using coq
+      dependencies = {
+      -- main one
+      { "ms-jpq/coq_nvim", branch = "coq" },
+
+      -- 9000+ Snippets
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+      -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+      -- Need to **configure separately**
+      { 'ms-jpq/coq.thirdparty', branch = "3p" }
+      -- - shell repl
+      -- - nvim lua api
+      -- - scientific calculator
+      -- - comment banner
+      -- - etc
+    },
+    init = function()
+      vim.g.coq_settings = {
+        auto_start = true, -- if you want to start COQ at startup
+        -- Your COQ settings here
+      }
+    end,
+
     config = function()
       local lspconfig = require("lspconfig")
 
