a4b29976d3bdf3c92c6462263a8c38892c1bed9c
nekomangini <encar.luis.salazar@gmail.com>
2024-09-21 15:50:41 +0800

    configured to work on vue files

--- lua/plugins/comment-nvim.lua
+++ lua/plugins/comment-nvim.lua
@@ -3,8 +3,33 @@ return {
     'numToStr/Comment.nvim',
     opts = {
       -- add any options here
+      --  pre_hook = function(ctx)
+      --    local U = require('Comment.utils')
+
+      --    -- Determine whether to use // or <!-- --> based on file type
+      --    local line = vim.api.nvim_get_current_line()
+
+      --    if line:match("^%s*<script") then
+      --      return "// "
+      --    elseif line:match("^%s*<template") then
+      --      return "<!---->"
+      --    end
+      --  end,
     },
     config = function()
+      local ft = require('Comment.ft')
+
+      -- Set comment strings for different file types
+      ft.set('vue', {
+        line = {
+          script = '//%s',      -- Line comment for <script>
+          template = '<!---->', -- Line comment for <template>
+        },
+        block = {
+          script = '/*%s*/',    -- Block comment for <script>
+          template = '<!---->', -- Block comment for <template>
+        },
+      })
       require('Comment').setup({
         mappings = {
           basic = false, -- Disable the default `gcc`, `gc` mappings
