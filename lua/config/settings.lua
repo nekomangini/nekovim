9ef9f44158c785fc61358e1ac0bce550d20a9521
nekomangini <encar.luis.salazar@gmail.com>
2024-09-21 15:53:12 +0800

    configured to work on vue files

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,10 +1,22 @@
 -- File: ~/.config/nvim/lua/config/settings.lua
--- Set tab width for Lua files to 2 spaces
+-- Lua settings
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = function()
     vim.opt_local.tabstop = 2      -- Number of spaces a <Tab> counts for
     vim.opt_local.shiftwidth = 2   -- Number of spaces to use for each step of (auto)indent
     vim.opt_local.expandtab = true -- Convert tabs to spaces
+  end,
+})
+
+-- Vue settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "vue",
+  callback = function()
+    vim.opt_local.commentstring = "// %s"
+    -- Set tab space and indentation options
+    vim.opt.tabstop = 2        -- Number of spaces that a <Tab> counts for
+    vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
+    vim.opt.expandtab = true   -- Use spaces instead of tabs
   end,
 })
