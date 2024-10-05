1c1b34455f85e1abc439e1305798a3dc2da188d6
nekomangini <encar.luis.salazar@gmail.com>
2024-10-05 20:03:03 +0800

    added debugger for flutter

--- lua/plugins/dart-tools.lua
+++ lua/plugins/dart-tools.lua
@@ -7,6 +7,21 @@ return {
       'stevearc/dressing.nvim',   -- optional for vim.ui.select
       -- When you run FlutterDevices a popup will show up and that is the dressing.nvim
     },
-    config = true,
+    -- config = true,
+    config = function()
+      require("flutter-tools").setup({
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          register_configurations = function(_)
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        lsp = {
+          on_attach = on_attach,  -- Use your LSP on_attach function
+          capabilities = capabilities,  -- Use your LSP capabilities
+        },
+      })
+    end 
   }
 }
