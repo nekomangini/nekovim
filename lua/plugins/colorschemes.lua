6eee165ea428e56cc0bdfbc98d48b543e96e2ab4
nekomangini <encar.luis.salazar@gmail.com>
2024-10-18 07:42:51 +0800

    added new theme: nordic

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -147,7 +147,7 @@ return {
         style = "multiplex",                                      -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
         toggle_style_key = nil,                                   -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
         toggle_style_list = { "vulgaris", "multiplex", "light" }, -- List of styles to toggle between
-        transparent = true,                                       -- Show/hide background
+        transparent = false,                                      -- Show/hide background
         dim_inactive = false,                                     -- Dim inactive windows/buffers
         term_colors = true,                                       -- Change terminal color as per the selected theme style
         ending_tildes = false,                                    -- Show the end-of-buffer tildes. By default they are hidden
@@ -195,7 +195,7 @@ return {
     config = function()
       require("cyberdream").setup({
         -- Enable transparent background
-        transparent = true,
+        transparent = false,
 
         -- Enable italics comments
         italic_comments = false,
@@ -243,5 +243,64 @@ return {
         },
       })
     end
-  }
-}
+  },
+  -- nordic
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').setup({
+        -- This callback can be used to override the colors used in the base palette.
+        on_palette = function(palette) end,
+        -- This callback can be used to override the colors used in the extended palette.
+        after_palette = function(palette) end,
+        -- This callback can be used to override highlights before they are applied.
+        on_highlight = function(highlights, palette) end,
+        -- Enable bold keywords.
+        bold_keywords = false,
+        -- Enable italic comments.
+        italic_comments = true,
+        -- Enable editor background transparency.
+        transparent = {
+          -- Enable transparent background.
+          bg = false,
+          -- Enable transparent background for floating windows.
+          float = false,
+        },
+        -- Enable brighter float border.
+        bright_border = false,
+        -- Reduce the overall amount of blue in the theme (diverges from base Nord).
+        reduced_blue = true,
+        -- Swap the dark background with the normal one.
+        swap_backgrounds = false,
+        -- Cursorline options.  Also includes visual/selection.
+        cursorline = {
+          -- Bold font in cursorline.
+          bold = false,
+          -- Bold cursorline number.
+          bold_number = true,
+          -- Available styles: 'dark', 'light'.
+          theme = 'dark',
+          -- Blending the cursorline bg with the buffer bg.
+          blend = 0.85,
+        },
+        noice = {
+          -- Available styles: `classic`, `flat`.
+          style = 'classic',
+        },
+        telescope = {
+          -- Available styles: `classic`, `flat`.
+          style = 'flat',
+        },
+        leap = {
+          -- Dims the backdrop when using leap.
+          dim_backdrop = false,
+        },
+        ts_context = {
+          -- Enables dark background for treesitter-context window
+          dark_background = true,
+        }
+      })
+    end
+  } }
