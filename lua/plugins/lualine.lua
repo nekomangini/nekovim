a6058285b2adf1dca98a1701e17f48183cbeaaf8
nekomangini <encar.luis.salazar@gmail.com>
2024-12-16 10:50:52 +0800

    config update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,77 +1,77 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = 'palenight', --everforest, powerline, palenight
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        }
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        -- lualine_c = { 'filetype', 'diff', 'diagnostics' },
-        lualine_c = {
-          {
-            'filename',
-            file_status = true, -- Displays file status (modified, read-only)
-            path = 1,           -- 0: Just the filename
-            -- 1: Relative path
-            -- 2: Absolute path
-            -- 3: Filename and parent dir
-            shorting_target = 40, -- Shortens path if it's longer than 40 characters
-            symbols = {
-              modified = ' ●', -- Text to show when the file is modified
-              readonly = ' ', -- Text to show when the file is non-modifiable or readonly
-              unnamed = '[No Name]' -- Text to show for unnamed buffers
-            }
-          },
-          'filetype',
-          'diff',
-          'diagnostics'
-        },
-        -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        -- lualine_x = { 'buffers' },
-        -- show @recording messages(bottom right side)
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          }
-        },
-        lualine_y = { 'location' },
-        lualine_z = { 'progress' },
-        -- lualine_z = { '%=', '%t%m', '%3p' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = { 'nvim-tree' } -- changes statusline appearance for nvim-tree
-    }
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "cyberdream", --everforest, powerline, palenight, gruvbox, cyberdream
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				-- lualine_c = { 'filetype', 'diff', 'diagnostics' },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (modified, read-only)
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Filename and parent dir
+						shorting_target = 40, -- Shortens path if it's longer than 40 characters
+						symbols = {
+							modified = " ●", -- Text to show when the file is modified
+							readonly = " ", -- Text to show when the file is non-modifiable or readonly
+							unnamed = "[No Name]", -- Text to show for unnamed buffers
+						},
+					},
+					"filetype",
+					"diff",
+					"diagnostics",
+				},
+				-- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				-- lualine_x = { 'buffers' },
+				-- show @recording messages(bottom right side)
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_y = { "location" },
+				lualine_z = { "progress" },
+				-- lualine_z = { '%=', '%t%m', '%3p' },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "nvim-tree" }, -- changes statusline appearance for nvim-tree
+		})
+	end,
 }
 -- return {
 --   'nvim-lualine/lualine.nvim',
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,83 +1,87 @@
 return {
-  -- lazy.nvim
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      local noice = require("noice")
-      noice.setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        presets = {
-          -- you can enable a preset by setting it to true, or a table that will override the preset config
-          -- you can also add custom presets that you can enable/disable with enabled=true
-          bottom_search = false,        -- use a classic bottom cmdline for search
-          command_palette = false,      -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = true,            -- enables an input dialog for inc-rename.nvim
-        },
-        views = {
-          -- minimalist setup
-          -- cmdline_popup = {
-          --   border = {
-          --     style = "none",
-          --     padding = { 2, 3 },
-          --   },
-          --   filter_options = {},
-          --   win_options = {
-          --     winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-          --   },
-          -- },
-        },
-        routes = {
-          -- Hides search messages count
-          {
-            filter = {
-              event = "msg_show",
-              kind = "search_count",
-            },
-            opts = { skip = true },
-          },
-          -- Filter out the "ready" notification
-          {
-            filter = {
-              event = "msg_show",
-              find = "ready",
-            },
-            opts = { skip = true },
-          },
-        },
-        messages = {
-          -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-          -- This is a current Neovim limitation.
-          enabled = true, -- enables the Noice messages UI
-          -- available options for view = notify, split, vsplit, popup, mini, cmdline, cmdline_popup, cmdline_output, messages, confirm, hover
-          -- view = "notify", -- default view for messages
-          view = "mini",               -- change this to see the resession popup
-          view_error = "notify",       -- view for errors
-          view_warn = "notify",        -- view for warnings
-          view_history = "messages",   -- view for :messages
-          view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-        },
-
-      })
-    end
-  }
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			local noice = require("noice")
+			-- fix: Gruvbox notification warning message
+			local notify = require("notify")
+			notify.setup({
+				background_colour = "#000000",
+			})
+			noice.setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						-- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					-- you can enable a preset by setting it to true, or a table that will override the preset config
+					-- you can also add custom presets that you can enable/disable with enabled=true
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = false, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				},
+				views = {
+					-- minimalist setup
+					-- cmdline_popup = {
+					--   border = {
+					--     style = "none",
+					--     padding = { 2, 3 },
+					--   },
+					--   filter_options = {},
+					--   win_options = {
+					--     winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+					--   },
+					-- },
+				},
+				routes = {
+					-- Hides search messages count
+					{
+						filter = {
+							event = "msg_show",
+							kind = "search_count",
+						},
+						opts = { skip = true },
+					},
+					-- Filter out the "ready" notification
+					{
+						filter = {
+							event = "msg_show",
+							find = "ready",
+						},
+						opts = { skip = true },
+					},
+				},
+				messages = {
+					-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+					-- This is a current Neovim limitation.
+					enabled = true, -- enables the Noice messages UI
+					-- available options for view = notify, split, vsplit, popup, mini, cmdline, cmdline_popup, cmdline_output, messages, confirm, hover
+					-- view = "notify", -- default view for messages
+					view = "mini", -- change this to see the resession popup
+					view_error = "notify", -- view for errors
+					view_warn = "notify", -- view for warnings
+					view_history = "messages", -- view for :messages
+					view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+				},
+			})
+		end,
+	},
 }
