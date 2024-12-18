130df5e03f9aff2bac980c5cd92e9228bef30dea
nekomangini <encar.luis.salazar@gmail.com>
2024-12-18 14:12:06 +0800

    added reactjs and vuejs support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,27 +11,152 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     config = function()
-      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
       local default_setup = function(server)
-        require('lspconfig')[server].setup({
+        require("lspconfig")[server].setup({
           capabilities = lsp_capabilities,
         })
       end
       require("mason-lspconfig").setup({
         -- auto_install = true, -- automatically install a language
-        ensure_installed = { "gopls", "lua_ls", "rust_analyzer", "taplo", "ts_ls" },
+        ensure_installed = {
+          "cssls",
+          "emmet_ls",
+          "eslint",
+          "gopls",
+          "html",
+          "lua_ls",
+          -- "prettier",
+          "rust_analyzer",
+          "taplo",
+          "ts_ls",
+          "volar",
+          "vtsls",
+        },
         handlers = {
           default_setup,
-          require("lspconfig").lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }, -- Avoid undefined 'vim'
+          -- Lua LSP specific configuration
+          ["lua_ls"] = function()
+            require("lspconfig").lua_ls.setup({
+              capabilities = lsp_capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                  },
                 },
               },
-            },
-          })
+            })
+          end,
 
+          -- TODO:
+          -- Vue (Volar) configuration with Takeover Mode
+          ["volar"] = function()
+            require("lspconfig").volar.setup({
+              filetypes = {
+                "vue",
+              },
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+              init_options = {
+                vue = {
+                  hybridMode = false, -- Full Takeover Mode
+                },
+                typescript = {
+                  tsdk = "/usr/local/lib/node_modules/typescript/lib",
+                },
+                autoUseWorkspaceTsdk = true,
+                suggestFromUnimportedLibraries = true,
+                preferences = {
+                  importModuleSpecifier = "non-relative",
+                },
+              },
+              on_new_config = function(new_config, new_root_dir)
+                local function find_typescript_lib(root)
+                  local possible_paths = {
+                    root .. "/node_modules/typescript/lib",
+                    "/usr/local/lib/node_modules/typescript/lib",
+                    vim.fn.expand("~/.npm/lib/node_modules/typescript/lib"),
+                    vim.fn.expand("~/.local/lib/node_modules/typescript/lib"),
+                  }
+
+                  for _, path in ipairs(possible_paths) do
+                    if vim.fn.filereadable(path .. "/typescript.js") == 1 then
+                      return path
+                    end
+                  end
+
+                  return nil
+                end
+
+                local lib_path = find_typescript_lib(new_root_dir)
+                if lib_path then
+                  new_config.init_options.typescript.tsdk = lib_path
+                end
+              end,
+            })
+          end,
+
+          -- VTSLS configuration
+          -- ["vtsls"] = function()
+          --   require("lspconfig").vtsls.setup({
+          --     filetypes = {
+          --       "vue",
+          --     },
+          --     settings = {
+          --       vtsls = {
+          --         autoUseWorkspaceTsdk = true,
+          --         globalPlugins = {
+          --           {
+          --             name = "@vue/typescript-plugin",
+          --             location = vim.fn.expand(
+          --               "~/.local/lib/node_modules/@vue/typescript-plugin"
+          --             ),
+          --             languages = { "vue" },
+          --           },
+          --         },
+          --       },
+          --       typescript = {
+          --         preferences = {
+          --           importModuleSpecifier = "non-relative",
+          --         },
+          --       },
+          --     },
+          --   })
+          -- end,
+
+          -- ESLint configuration
+          -- ["eslint"] = function()
+          --   require("lspconfig").eslint.setup({
+          --     on_attach = function(client, bufnr)
+          --       vim.api.nvim_create_autocmd("BufWritePre", {
+          --         buffer = bufnr,
+          --         command = "EslintFixAll",
+          --       })
+          --     end,
+          --   })
+          -- end,
+        },
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.goimports,
+          null_ls.builtins.completion.spell,
+          null_ls.builtins.diagnostics.eslint,
+
+          -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
         },
       })
     end,
@@ -53,12 +178,15 @@ return {
       lsp_zero.on_attach(function(event)
         -- LSP keymaps that are only active when LSP is attached
         local opts = { buffer = event.buf }
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+        vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<cr>", opts)
+        vim.keymap.set("n", "gi", "<cmd>Lspsaga finder imp<cr>", opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
       end)
 
@@ -82,7 +210,6 @@ return {
       -- })
       --
       -- local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 
       ---
       -- moved to mason-lsp handlers
@@ -126,15 +253,21 @@ return {
       -- autocompletion config
       ---
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       -- local cmp_action = require("lsp-zero").cmp_action()
 
       cmp.setup({
         -- Sources for autocompletion
         sources = {
           { name = "nvim_lsp" }, -- Use LSP for autocompletion
-          { name = "luasnip" },  -- luasnip
-          { name = "buffer" },   -- Suggest words from the current buffer
-          { name = "path" },     -- Suggest file paths
+          { name = "luasnip" }, -- luasnip
+          { name = "buffer" }, -- Suggest words from the current buffer
+          { name = "path" }, -- Suggest file paths
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
         mapping = cmp.mapping.preset.insert({
           -- `Enter` key to confirm completion
@@ -143,13 +276,8 @@ return {
           -- Ctrl+Space to trigger completion menu
           ["<C-Space>"] = cmp.mapping.complete(),
 
-          ['<C-e>'] = cmp.mapping.abort(),
+          ["<C-e>"] = cmp.mapping.abort(),
         }),
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         -- Enable documentation popups
         window = {
           documentation = cmp.config.window.bordered(), -- Nice border for documentation
@@ -177,6 +305,22 @@ return {
       --     },
       --   }),
       -- })
+    end,
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons",  -- optional
+    },
+    event = "LspAttach",              -- Load when LSP attaches
+    config = function()
+      require("lspsaga").setup({
+        ui = {
+          enable = true,
+          sign = true,
+        },
+      })
     end,
   },
 
