d3a68d97948743d372889e06c0114734b2738b7a
nekomangini <encar.luis.salazar@gmail.com>
2024-12-18 14:11:14 +0800

    config update

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -4,5 +4,26 @@ return {
     config = function()
       require("autoclose").setup()
     end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "xml"
+    },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,          -- Auto close tags
+          enable_rename = true,         -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        }
+      })
+    end,
   }
 }
