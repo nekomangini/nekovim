ad816122f48d5f79f05fc5cd5506d248abd7b0ae
nekomangini <encar.luis.salazar@gmail.com>
2025-01-15 11:19:29 +0800

    Merged keymaps.lua and yanky-nvim.lua

---
+++ lua/plugins/yanky-nvim.lua
@@ -0,0 +1,16 @@
+return {
+	"gbprod/yanky.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		system_clipboard = {
+			sync_with_ring = true, -- This enables automatic sync with system clipboard
+		},
+		highlight = {
+			on_put = true,
+			on_yank = true,
+			timer = 160,
+		},
+	},
+}
